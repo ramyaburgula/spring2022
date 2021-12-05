af0ad9c36f015c36cd8f766772c5d7f340850c55
EC2 Default User <ec2-user@ip-172-31-1-50.ec2.internal>
2021-12-05 08:17:27 +0000

    assignment

--- Chapter-1-Introduction/ex2-flask.py
+++ Chapter-1-Introduction/ex2-flask.py
@@ -1,11 +1,19 @@
-# Basic Flask Code
-
-from flask import Flask
+# basic flask code
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'Hello World'
-   
+@app.route('/success/<name>')
+def success(name):
+  return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+	   user = request.args.get('nm')
+	   return redirect(url_for('success',name = user))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug = True)
+   app.run(debug = True)
